5b71a41d211d571318e5a541e1adcf02e7c065fb
nanoo17 <marianojrodriguez10@gmail.com>
2023-09-21 23:38:36 -0300

    poniendo todo en su lugar...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HistorialComponent } from './components/historial/historial.component';
+import { PrincipalComponent } from './components/principal/principal.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home', component: PrincipalComponent },
+  { path: 'historial', component: HistorialComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/layouts/header/header.component';
+import { HistorialComponent } from './components/historial/historial.component';
+import { PrincipalComponent } from './components/principal/principal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HistorialComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule,
