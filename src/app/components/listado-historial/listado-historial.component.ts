5a0d61fef53862a8121617f487217a2d352df022
nanoo17 <marianojrodriguez10@gmail.com>
2023-09-25 15:14:33 -0300

    historial-listado

---
+++ src/app/components/listado-historial/listado-historial.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-historial',
+  templateUrl: './listado-historial.component.html',
+  styleUrls: ['./listado-historial.component.css']
+})
+export class ListadoHistorialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
